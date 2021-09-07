69ea4c77290e2ffd1034533e17c3ddd80ed880ab
SandraCalero <3358@holbertonschool.com>
2021-09-07 00:10:30 -0500

    add a function in C that checks if a singly linked list has a cycle in it

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: Pointer to head of a singly linked list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow_ptr = list;
+	listint_t *fast_ptr = list;
+
+	if (list == NULL)
+		return (0);
+	while (fast_ptr != NULL && fast_ptr->next != NULL)
+	{
+		fast_ptr = fast_ptr->next->next;
+		slow_ptr = slow_ptr->next;
+		if (fast_ptr == slow_ptr)
+			return (1);
+	}
+	return (0);
+}
