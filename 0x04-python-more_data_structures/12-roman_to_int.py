bfd07079854b9362a13d44a95b6e2f8010f79f45
SandraCalero <3358@holbertonschool.com>
2021-09-16 00:23:59 -0500

    add a function that converts a Roman numeral to an intege

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    num_directory = {'I': 1, 'V': 5, 'X': 10,
-                     'L': 50, 'C': 100, 'D': 500, 'M': 100}
-    num_list = []
-    for key in roman_string:
-        num_list.append(num_directory[key])
-    num_list.reverse()
-    i = 0
-    sum = num_list[0]
-    while(i < len(num_list) - 1):
-        if(num_list[i] > num_list[i + 1]):
-            sum = sum - num_list[i+1]
-        else:
-            sum = sum + num_list[i+1]
-    return sum
+    if roman_string is None or roman_string == "":
+        return 0
+    elif type(roman_string) is str:
+        num_directory = {'I': 1, 'V': 5, 'X': 10,
+                         'L': 50, 'C': 100, 'D': 500, 'M': 100}
+        num_list = []
+        for key in roman_string:
+            num_list.append(num_directory[key])
+        num_list.reverse()
+        i = 0
+        sum = num_list[0]
+        while(i < len(num_list) - 1):
+            if(num_list[i] > num_list[i + 1]):
+                sum = sum - num_list[i+1]
+            else:
+                sum = sum + num_list[i+1]
+            i += 1
+        return sum
+    else:
+        return 0
