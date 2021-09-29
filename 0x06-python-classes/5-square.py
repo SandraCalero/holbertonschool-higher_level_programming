a71530e70a566f09bb87154a03493f628b720e26
SandraCalero <3358@holbertonschool.com>
2021-09-29 13:20:58 -0500

    add a class that prints a aquere given a size

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Class that defines a square"""
+
+
+class Square:
+    """Defines the object of class square"""
+    def __init__(self, size=0):
+        """Defines the squere atributes"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+
+    @property
+    def size(self):
+        """Gets the squere size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the squere size"""
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        """Calculates the squere area"""
+        square_area = self.__size * self.__size
+        return square_area
+
+    def my_print(self):
+        """Prints in stdout the square with the character #"""
+        if self.__size == 0:
+            print("")
+        else:
+            square_row = '#' * self.__size
+            for i in range(0, self.__size):
+                print ("{}".format(square_row))
