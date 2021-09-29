8ed22b36a13c55b392f8359a833854759ef6687d
SandraCalero <3358@holbertonschool.com>
2021-09-29 00:07:23 -0500

    add a class to ccess and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Class that defines a square"""
+
+
+class Square:
+    """Defines the object of class square"""
+    def __init__(self, size=0):
+        """Defines the squere atributes"""
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+
+    @property
+    def size(self):
+        """Gets the squere size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the squere size"""
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+
+    def area(self):
+        """Calculates the squere area"""
+        square_area = self.__size * self.__size
+        return square_area
