ad33537282b20243fc97682d084365dbefe04af7
SandraCalero <3358@holbertonschool.com>
2021-10-15 22:13:49 -0500

    add modules base and rectangle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module base.py contain the class that is the “base” of all other classes"""
+
+
+class Base:
+    """Base class
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Constructor of instances
+
+        Args:
+            id (int): identification number of each instance.
+            Defaults to None.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+"""Module rectangle.py that contains class Rectangle
+that inherits from Base
+"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Rectangle class
+
+    Args:
+        Base ([base]): Super class
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor
+
+        Args:
+            width (int): The width of the rectangle
+            height (int): The height of the rectangle
+            x (int): [description]. Defaults to 0.
+            y (int): [description]. Defaults to 0.
+            id (int): [description]. Defaults to None.
+        """
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """get width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set width"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set height"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """get x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """set x"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """get y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """set y"""
+        self.__y = value
