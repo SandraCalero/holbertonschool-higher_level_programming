06b1202ebce377cfd384939e803dd756ee6d3608
SandraCalero <3358@holbertonschool.com>
2021-10-18 14:43:59 -0500

    add tets cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -37,14 +37,14 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             Rectangle("2", 10)
 
-    def test_TypeError_height(self):
-        """Test error raised by providing an invalid type height"""
-        with self.assertRaises(TypeError):
+    def test_TypeErrorMessage_height(self):
+        """Test error and message raised by providing an invalid type height"""
+        with self.assertRaisesRegex(TypeError, "height must be an integer"):
             Rectangle(10, 2.5)
 
-    def test_TypeError_x(self):
-        """Test error raised by providing an invalid type x"""
-        with self.assertRaises(TypeError):
+    def test_TypeErrorMessage_x(self):
+        """Test error and message raised by providing an invalid type x"""
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
             Rectangle(10, 2, [1])
 
     def test_TypeError_y(self):
@@ -52,9 +52,9 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(TypeError):
             Rectangle(10, 2, 1, 3.56)
 
-    def test_ValueError_width(self):
-        """Test error raised by providing a negative width"""
-        with self.assertRaises(ValueError):
+    def test_ValueErrorMessage_width(self):
+        """Test error and message raised by providing a negative width"""
+        with self.assertRaisesRegex(ValueError, "width must be > 0"):
             Rectangle(-2, 10)
 
     def test_ValueError_height(self):
@@ -67,10 +67,14 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(ValueError):
             Rectangle(10, 2, -1)
 
-    def test_ValueError_y(self):
-        """Test error raised by providing a negative type y"""
-        with self.assertRaises(ValueError):
+    def test_ValueErrorMessage_y(self):
+        """Test error and message raised by providing a negative type y"""
+        with self.assertRaisesRegex(ValueError, "y must be >= 0"):
             Rectangle(10, 2, 1, -3)
+
+    def test_area(self):
+        r2 = Rectangle(6, 5)
+        self.assertEqual(r2.area(), 30)
 
 
 if __name__ == "__main__":
