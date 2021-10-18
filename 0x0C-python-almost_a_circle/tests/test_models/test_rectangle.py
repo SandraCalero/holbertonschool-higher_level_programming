da72396d05faab94349167b5f1053f698e1ba055
SandraCalero <3358@holbertonschool.com>
2021-10-18 14:20:52 -0500

    add test cases and fix pycodestyle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -4,14 +4,53 @@
 import unittest
 from models.rectangle import Rectangle
 
+
 class TestRectangle(unittest.TestCase):
     """Class to test the class Rectangle
     """
-    def test_id_subclass(self):
-        r1 = Rectangle(10, 2, 0, 0, 12)
-        self.assertEqual(r1.id, 12)
-    
+    def setUp(self):
+        """Creates the instance that can be used in the following tests"""
+        self.r1 = Rectangle(10, 2, 0, 0, 12)
 
+    def test_id(self):
+        """Test id of Rectangle r1"""
+        self.assertEqual(self.r1.id, 12)
+
+    def test_width(self):
+        """Test width of Rectangle r1"""
+        self.assertEqual(self.r1.width, 10)
+
+    def test_height(self):
+        """Test height of Rectangle r1"""
+        self.assertEqual(self.r1.height, 2)
+
+    def test_x(self):
+        """Test x of Rectangle r1"""
+        self.assertEqual(self.r1.x, 0)
+
+    def test_y(self):
+        """Test y of Rectangle r1"""
+        self.assertEqual(self.r1.y, 0)
+
+    def test_TypeError_width(self):
+        """Test error raised by providing an invalid type width"""
+        with self.assertRaises(TypeError):
+            Rectangle("2", 10)
+
+    def test_TypeError_height(self):
+        """Test error raised by providing an invalid type height"""
+        with self.assertRaises(TypeError):
+            Rectangle(10, 2.5)
+
+    def test_TypeError_x(self):
+        """Test error raised by providing an invalid type x"""
+        with self.assertRaises(TypeError):
+            Rectangle(10, 2, [1])
+
+    def test_TypeError_y(self):
+        """Test error raised by providing an invalid type y"""
+        with self.assertRaises(TypeError):
+            Rectangle(10, 2, 1, 3.56)
 
 
 if __name__ == "__main__":
