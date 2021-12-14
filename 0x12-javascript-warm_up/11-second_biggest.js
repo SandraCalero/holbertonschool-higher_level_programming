32f362e174b86aebcf6aa01a46513783d1794e4f
Sandra Calero <3358@holbertonschool.com>
2021-12-13 22:15:03 -0500

    add a script that searches the second biggest integer in the list of arguments.

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
 /// This script is searching the second biggest integer in the list of arguments
-const process = require('process');
+const process = require("process");
 const array = process.argv.slice(2, process.argv.length);
 const arrayLen = array.length;
 if (arrayLen <= 1) {
-  console.log('0');
+  console.log(0);
 } else {
   array.sort();
-  for (let idx = arrayLen - 2; idx >= 0; idx--) {
-    if (array[idx] !== array[idx + 1]) {
-      console.log(parseInt(array[idx]));
-      break;
-    }
-  }
+  console.log(array);
+  console.log(parseInt(array[arrayLen - 2]));
 }
