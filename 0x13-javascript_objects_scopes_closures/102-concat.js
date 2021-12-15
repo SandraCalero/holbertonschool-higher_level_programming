ff99d782cd6878ff6177b391f84e3b349e1300b2
Sandra Calero <3358@holbertonschool.com>
2021-12-14 23:21:27 -0500

    add a script that concats 2 files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -6,4 +6,4 @@ const sourceFile1 = fs.readFileSync(process.argv[2], 'utf8');
 const sourceFile2 = fs.readFileSync(process.argv[3], 'utf8');
 const destFile = process.argv[4];
 
-fs.appendFileSync(destFile, sourceFile1 + '\n' + sourceFile2 + '\n', 'utf8');
+fs.appendFileSync(destFile, sourceFile1 + sourceFile2, 'utf8');
