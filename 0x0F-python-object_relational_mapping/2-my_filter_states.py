b62b3d864d9595c31ad0e63688452abf1ff3adf8
Sandra Calero <3358@holbertonschool.com>
2021-12-18 13:51:34 -0500

    fix binary

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id".
+    cur.execute("SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id".
                 format(argv_filter))
     query_rows = cur.fetchall()
     for row in query_rows:
