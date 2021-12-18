d5a856f4d1c76292dceb8dc99194172109482e5f
Sandra Calero <3358@holbertonschool.com>
2021-12-18 18:04:58 -0500

    add nullable id false

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,12 +3,13 @@
 Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
     __tablename__ = "states"
-    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
