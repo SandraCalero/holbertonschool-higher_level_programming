1901732b4fe08ec84cffb3563400baefa1f4c8de
Sandra Calero <3358@holbertonschool.com>
2021-12-18 17:57:01 -0500

    add sheabang

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,7 +3,7 @@
 Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
