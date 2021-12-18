2f0d4a591874bc398f3c4c602e16479bb945c0e1
Sandra Calero <3358@holbertonschool.com>
2021-12-18 16:15:28 -0500

    add a script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This script is takeing in the name of a state as an argument and lists
+all cities of that state, using the database hbtn_0e_4_usa
+"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3], charset="utf8")
+    cur = conn.cursor()
+
+    cur.execute("SELECT cities.name FROM cities INNER JOIN states on\
+        states.id = cities.state_id WHERE BINARY states.name = %s\
+        ORDER BY cities.id", [argv[4]])
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
