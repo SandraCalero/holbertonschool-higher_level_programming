a9ae2e9b5fc850add6e2092d60423b7c0066e889
Sandra Calero <3358@holbertonschool.com>
2022-01-08 01:09:59 -0500

    add a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# This script is taking in a URL, sends a POST request to the passed URL, and displays the body of the response
+# A variable email must be sent with the value test@gmail.com
+# A variable subject must be sent with the value I will always be here for PLD
+curl -X POST -d 'email=test@gmail.com&subject=I will always be here for PLD' "$1"
+
+# The -X option specifies which HTTP request method will be used when communicating with the remote server.
+# The -d option Sends the specified data in a POST request to the HTTP server, in the same way that a browser
+# does when a user has filled in an HTML form and presses the submit button
