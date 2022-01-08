5f6a54e8ca9dcb5723103f52e8b1aeea7d95354e
Sandra Calero <3358@holbertonschool.com>
2022-01-08 12:56:55 -0500

    update command

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script is taking in a URL as an argument, sends a GET request to the URL, and displays the body of the response
 # A header variable X-School-User-Id must be sent with the value 98
-curl -sH GET "$1?X-School-User-Id=98"
+curl -sH "X-School-User-Id: 98" "$1"
