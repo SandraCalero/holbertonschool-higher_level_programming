781d8c7e5ad7e8e1eab355380a2700ba06757aee
Sandra Calero <3358@holbertonschool.com>
2022-01-08 13:38:05 -0500

    update conditional

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,7 +4,7 @@
 
 
 def find_peak(list_of_integers):
-    if isinstance(list_of_integers, list) and len(list_of_integers) > 0:
+    if type(list_of_integers) == list and len(list_of_integers) > 0:
         list_of_integers.sort()
         return list_of_integers[-1]
     return None
