7c9c2417fe006b24cc05208b351feb7b3f9bd0e4
Sandra Calero <3358@holbertonschool.com>
2022-01-08 13:40:39 -0500

    add docstring

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,6 +4,14 @@
 
 
 def find_peak(list_of_integers):
+    """Finds a peak in a list of unsorted integers.
+
+    Args:
+        list_of_integers ([list]): unsorted list of integers
+
+    Returns:
+        a peak
+    """
     if type(list_of_integers) == list and len(list_of_integers) > 0:
         list_of_integers.sort()
         return list_of_integers[-1]
