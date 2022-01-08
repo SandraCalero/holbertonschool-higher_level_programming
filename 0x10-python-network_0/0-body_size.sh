ea48cead8450fcadf2f5e70f8b963558c4a801b7
Sandra Calero <3358@holbertonschool.com>
2022-01-08 12:48:51 -0500

    update command

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # This script is taking in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
-
-# Another option: curl -s "$1" | wc -c
+curl -s "$1" | wc -c
