f949daa958405f06be2d9ddc86915c7efbd297c6
Sandra Calero <3358@holbertonschool.com>
2022-01-08 13:11:15 -0500

    update command

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -3,7 +3,3 @@
 # A variable email must be sent with the value test@gmail.com
 # A variable subject must be sent with the value I will always be here for PLD
 curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
-
-# The -X option specifies which HTTP request method will be used when communicating with the remote server.
-# The -d option Sends the specified data in a POST request to the HTTP server, in the same way that a browser
-# does when a user has filled in an HTML form and presses the submit button
